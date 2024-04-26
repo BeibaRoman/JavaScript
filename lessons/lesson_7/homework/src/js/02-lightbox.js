4dfa547f76334c715e09cbbaa9d3df487ad4955a
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2024-04-26 22:17:21 +0300

    add gallery simplelightbox

--- lessons/lesson_7/homework/src/js/02-lightbox.js
+++ lessons/lesson_7/homework/src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+
+const markup = markupGalleryImage(galleryItems);
+
+galleryListEl.insertAdjacentHTML("afterbegin", markup);
+
+function markupGalleryImage(array) {
+  return array
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
